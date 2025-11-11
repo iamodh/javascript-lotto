f8e1b7ed411121a6a3fda6f9736638ac5d3eaefa
Donghwan Oh <imodh97@gmail.com>
2025-11-11 23:19:25 +0900

    test(lotto): 우승 로또 객체와의 협력을 위한 기능 테스트 추가

--- __tests__/models/entities/LottoTest.js
+++ __tests__/models/entities/LottoTest.js
@@ -13,18 +13,22 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('당첨 로또와 비교하여 자신의 등수를 계산한다.', () => {
+  test('보너스 번호의 포함 여부를 반환한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const BONUS_NUMBER = 6;
 
-    const mockWinningLotto = {
-      calculateMatchCount: (lottoNumbers) => {
-        return 5;
-      },
-      calculateBonusMatch: (lottoNumbers) => {
-        return true;
-      },
-    };
+    expect(lotto.contains(BONUS_NUMBER)).toBe(true);
+  });
 
-    expect(lotto.calculateRank(mockWinningLotto)).toBe(2);
+  test('당첨 로또 번호와 일치하는 번호의 개수를 반환한다', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 4, 5, 7];
+
+    expect(lotto.matchCount(winningNumbers)).toBe(5);
+  });
+
+  test('로또 번호를 오름차순으로 정렬하여 반환한다.', () => {
+    const lotto = new Lotto([6, 5, 4, 3, 2, 1]);
+    expect(lotto.getSortedNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
