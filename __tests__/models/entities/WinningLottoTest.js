d43217d420c1e161e83591e27a6d3948e57d30e3
Donghwan Oh <imodh97@gmail.com>
2025-11-11 15:06:08 +0900

    test(winningLotto): 로또 클래스와의 협력을 위한 테스트 추가

--- __tests__/models/entities/WinningLottoTest.js
+++ __tests__/models/entities/WinningLottoTest.js
@@ -1,4 +1,4 @@
-import WinningLotto from '/src/models/entities/WinningLotto';
+import WinningLotto from '../../../src/models/entities/WinningLotto';
 
 describe('당첨 로또 클래스 테스트', () => {
   test('당점 로또 번호의 개수가 6개가 아니라면 예외가 발생한다.', () => {
@@ -17,5 +17,20 @@ describe('당첨 로또 클래스 테스트', () => {
     expect(() => {
       new WinningLotto([1, 2, 3, 4, 5, 6], 6);
     }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호를 받아 자신 번호와 일치하는 번호의 개수를 반환한다.', () => {
+    const LOTTO_NUBMERS = [1, 2, 3, 4, 5, 6];
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 7], 6);
+
+    expect(winningLotto.calculateMatchCount(LOTTO_NUBMERS)).toBe(5);
+  });
+
+  test('로또 번호를 받아 자신의 보너스 번호와 일치하는 번호의 여부를 반환한다.', () => {
+    const LOTTO_NUBMERS = [1, 2, 3, 4, 5, 6];
+
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 7], 6);
+
+    expect(winningLotto.calculateBonusMatch(LOTTO_NUBMERS)).toBe(true);
   });
 });
