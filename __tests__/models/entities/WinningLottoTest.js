c42f4d834c68345ec073e734dd835c04a7ef59d5
Donghwan Oh <imodh97@gmail.com>
2025-11-11 23:23:57 +0900

    test(winningLotto): 로또 클래스와 협력을 위한 기능 테스트 추가

--- __tests__/models/entities/WinningLottoTest.js
+++ __tests__/models/entities/WinningLottoTest.js
@@ -19,18 +19,14 @@ describe('당첨 로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('로또 번호를 받아 자신 번호와 일치하는 번호의 개수를 반환한다.', () => {
-    const LOTTO_NUBMERS = [1, 2, 3, 4, 5, 6];
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 7], 6);
-
-    expect(winningLotto.calculateMatchCount(LOTTO_NUBMERS)).toBe(5);
-  });
-
-  test('로또 번호를 받아 자신의 보너스 번호와 일치하는 번호의 여부를 반환한다.', () => {
-    const LOTTO_NUBMERS = [1, 2, 3, 4, 5, 6];
+  test('로또의 등수를 계산한다.', () => {
+    const mockLotto = {
+      numbers: [1, 2, 3, 4, 5, 6],
+      matchCount: jest.fn().mockReturnValue(5),
+      contains: jest.fn().mockReturnValue(true),
+    };
 
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 7], 6);
-
-    expect(winningLotto.calculateBonusMatch(LOTTO_NUBMERS)).toBe(true);
+    expect(winningLotto.calculateRank(mockLotto)).toBe('SECOND');
   });
 });
