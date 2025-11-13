fd8c41cd864c311c0714c36ff019a9dd70ef878f
Donghwan Oh <imodh97@gmail.com>
2025-11-13 11:32:34 +0900

    LottoResult 객체에서 stats를 생성하는  책임 분리
    LottoResult는 stats와 purchasMoney를 전달 받아 수익률을 계산하는 책임을 가짐

--- __tests__/models/domains/LottoResultTest.js
+++ __tests__/models/domains/LottoResultTest.js
@@ -1,15 +1,9 @@
 import LottoResult from '../../../src/models/domains/LottoResult';
 
 describe('LottoResult 클래스 테스트', () => {
-  test('lottos와 winningLotto를 받아 우승 통계를 계산한다.', () => {
-    const mockLottos = [{ numbers: [1, 2, 3, 4, 5, 6] }];
-
-    const mockWinningLotto = {
-      calculateRank: jest.fn().mockReturnValue('FIFTH'),
-    };
-
-    const lottoResult = new LottoResult();
-    const EXPECTED_STATS = new Map([
+  test('구입 금액에 대한 수익률을 계산한다.', () => {
+    const PURCHASE_MONEY = 8000;
+    const fakeStats = new Map([
       ['FIRST', 0],
       ['SECOND', 0],
       ['THIRD', 0],
@@ -17,20 +11,8 @@ describe('LottoResult 클래스 테스트', () => {
       ['FIFTH', 1],
     ]);
 
-    const stats = lottoResult.calculateStatistics(mockLottos, mockWinningLotto);
-    expect(stats).toEqual(EXPECTED_STATS);
-  });
+    const lottoResult = new LottoResult(PURCHASE_MONEY, fakeStats);
 
-  test('구입 금액에 대한 수익률을 계산한다.', () => {
-    const mockLottos = [{ numbers: [1, 2, 3, 4, 5, 6] }];
-    const mockWinningLotto = {
-      calculateRank: jest.fn().mockReturnValue('FIFTH'),
-    };
-
-    const lottoResult = new LottoResult();
-    lottoResult.calculateStatistics(mockLottos, mockWinningLotto);
-    const PURCHASE_MONEY = 8000;
-
-    expect(lottoResult.calculateProfitRate(PURCHASE_MONEY)).toBeCloseTo(62.5);
+    expect(lottoResult.calculateProfitRate()).toBeCloseTo(62.5);
   });
 });
--- src/models/domains/LottoResult.js
+++ src/models/domains/LottoResult.js
@@ -1,39 +1,23 @@
 import PRIZE_CONFIG from '../../constants/prizeConfig.js';
 
-// 로또를 받아 rank를 입력
 class LottoResult {
-  #result = new Map([
-    ['FIRST', 0],
-    ['SECOND', 0],
-    ['THIRD', 0],
-    ['FOURTH', 0],
-    ['FIFTH', 0],
-  ]);
+  #purchaseMoney;
+  #stats;
 
-  calculateStatistics(lottos, winningLotto) {
-    for (const lotto of lottos) {
-      const rank = winningLotto.calculateRank(lotto);
-      this.#addRank(rank);
-    }
-    return this.#result;
+  constructor(purchaseMoney, stats) {
+    this.#purchaseMoney = purchaseMoney;
+    this.#stats = stats;
   }
 
-  #addRank(rank) {
-    if (rank === 'NONE') {
-      return;
-    }
-    this.#result.set(rank, this.#result.get(rank) + 1);
-  }
-
-  calculateProfitRate(purchaseMoney) {
+  calculateProfitRate() {
     const totalPrize = this.#calculateTotalPrize();
 
-    return Math.round((totalPrize / purchaseMoney) * 100 * 10) / 10;
+    return Math.round((totalPrize / this.#purchaseMoney) * 100 * 10) / 10;
   }
 
   #calculateTotalPrize() {
     let totalPrize = 0;
-    for (const [rank, count] of this.#result.entries()) {
+    for (const [rank, count] of this.#stats.entries()) {
       totalPrize += (PRIZE_CONFIG.MONEY[rank] || 0) * count;
     }
 
